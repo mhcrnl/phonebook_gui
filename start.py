5aa93ece5340cbe0c885cdc75b876dcca5d0c554
Michelle Levine <mjlevine18@gmail.com>
2016-03-19 13:46:14 -0700

    adding comments to the code. Adding a new menu item

--- start.py
+++ start.py
@@ -8,18 +8,36 @@ class windowClass(wx.Frame):
 
 
     def basicGUI(self):
+        # Creating the Panel to hold the stuff
+        panel = wx.Panel(self)
+
         # Create the menu bar
         menuBar = wx.MenuBar()
+
+        # Menu Items
         fileButton = wx.Menu()
-        exitItem = fileButton.Append(wx.ID_CLOSE, 'Exit Program', 'status message')
+        editButton = wx.Menu()
 
+        # Buttons in File Menu
+        exitItem = fileButton.Append(wx.ID_CLOSE, 'Exit', 'status message')
+
+        # Buttons in Edit Menu
+        copyItem = editButton.Append(wx.ID_COPY, 'Copy')
+
+        # Adding Menu Items to menuBar
         menuBar.Append(fileButton, 'File')
+        menuBar.Append(editButton, 'Edit')
 
+        # Initializing the menuBar
         self.SetMenuBar(menuBar)
 
+        # Binding the quitProgram function to the exit button
         self.Bind(wx.EVT_MENU, self.quitProgram, exitItem)
 
+        # Setting the title of the window
         self.SetTitle('Epic Window')
+
+        # Making the window actually show up
         self.Show(True)
 
     def quitProgram(self, e):
@@ -35,11 +53,3 @@ def main():
 
 
 main()
-
-
-
-
-
-
-
-
