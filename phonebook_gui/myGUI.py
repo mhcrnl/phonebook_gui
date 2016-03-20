1182e5f4b1c9d32429fc161a22c0945d041a109b
Michelle Levine <mjlevine18@gmail.com>
2016-03-20 16:41:08 -0700

    building out the visual aspects

---
+++ phonebook_gui/myGUI.py
@@ -0,0 +1,78 @@
+import wx
+
+class Frame(wx.Frame):
+    def __init__(self, title):
+        wx.Frame.__init__(self, None, title=title, size=(800,600))
+        panel = wx.Panel(self)
+
+        # ******************************************************************
+        # ******************* Menu Bar Creation ****************************
+        # ******************************************************************
+
+        # Create a Menu Bar
+        menuBar = wx.MenuBar()
+        # Create FileMenu as a menu option
+        fileMenu = wx.Menu()
+        # Add exitItem (Exit) as an option under File on MenuBar
+        exitItem = fileMenu.Append(wx.NewId(), 'Exit')
+        # Add fileMenu to actually show up on MenuBar
+        menuBar.Append(fileMenu, 'File')
+        # Setting the Menu Bar (Turning it on)
+        self.SetMenuBar(menuBar)
+
+        # Binding exitProgram action to exitItem
+        self.Bind(wx.EVT_MENU, self.exitProgram, exitItem)
+
+
+        # ******************************************************************
+        # ******************* Layout Setup *********************************
+        # ******************************************************************
+
+        # Setting up UI for adding a new contact area
+        wx.StaticBox(panel, label="Add a new contact", pos=(20,40), size=(300,200))
+
+        # creating add new contact labels and text boxes
+        wx.StaticText(panel, label="First Name:", pos=(40,70))
+        self.cFName = wx.TextCtrl(panel, size=(150, -1), pos=(150, 70))
+        wx.StaticText(panel, label="Last Name:", pos=(40,110))
+        self.cLName = wx.TextCtrl(panel, size=(150, -1), pos=(150, 110))
+        wx.StaticText(panel, label="Phone Number:", pos=(40,150))
+        self.cPhone = wx.TextCtrl(panel, pos=(150, 150), size=(150,-1))
+        wx.StaticText(panel, label="Email Address:", pos=(40,190))
+        self.cEmail = wx.TextCtrl(panel, size=(150, -1), pos=(150, 190))
+
+        # Setting up the table to display information
+        self.listCtrl = wx.ListCtrl(panel, size=(300, 400), pos=(350, 40), \
+                                     style=wx.LC_REPORT | wx.BORDER_SUNKEN)
+        # Adding columns to the table
+        self.listCtrl.InsertColumn(0, "ID")
+        self.listCtrl.InsertColumn(1, "First Name")
+        self.listCtrl.InsertColumn(2, "Last Name")
+
+
+
+        # ******************************************************************
+        # ******************* Buttons Creation *****************************
+        # ******************************************************************
+
+
+    def exitProgram(self, event):
+        # Creating yes/no dialog box
+        yesNoBox = wx.MessageDialog(None, 'Are you sure you want to quit?', 'Question', wx.YES_NO)
+        yesNoAnswer = yesNoBox.ShowModal()
+        yesNoBox.Destroy()
+
+        if yesNoAnswer == wx.ID_NO:
+            pass
+        if yesNoAnswer == wx.ID_YES:
+            # Terminate Program
+            self.Destroy()
+
+
+
+
+
+app = wx.App()
+frame = Frame('Basic Contact List')
+frame.Show()
+app.MainLoop()
---
+++ phonebook_gui/phonebook_database.py
@@ -0,0 +1,70 @@
+import sqlite3
+
+# Connect to database 'phonebook.db'
+conn = sqlite3.connect('phonebook.db')
+
+def createTable():
+    # Create a table named CHARACTER_INFO
+    conn.execute("CREATE TABLE if not EXISTS PHONE_BOOK ( \
+                 ID INTEGER PRIMARY KEY AUTOINCREMENT, \
+                 FIRST_NAME TEXT, \
+                 LAST_NAME TEXT, \
+                 PHONE_NUMBER TEXT, \
+                 EMAIL TEXT \
+                 );")
+
+def printData(data):
+    for row in data:
+        print "Id:", row[0]
+        print "First Name:", row[1]
+        print "Last Name:", row[2]
+        print "Phone Number:", row[3]
+        print "Email Address:", row[4], "\n"
+
+def newContact(fName,lName,phone,email):
+    # Create values part of sql command
+    val_str = "'{}', '{}', {}, '{}'".format(\
+        fName, lName, phone, email)
+
+    sql_str = "INSERT INTO PHONE_BOOK \
+        (FIRS_NAME, LAST_NAME, PHONE_NUMBER, EMAIL \
+        VALUES ({});".format(val_str)
+    print sql_str
+
+    conn.execute(sql_str)
+    conn.commit()
+    return conn.total_changes
+
+def viewAll():
+    # Create sql string
+    sql_str = "SELECT * from PHONE_BOOK"
+    cursor = conn.execute(sql_str)
+
+    # Get data from cursor in array
+    rows = cursor.fetchall()
+    return rows
+
+def updateContact(change_id,fName,lName,phone,email):
+    # Create values part of sql command
+    val_str = "FIRST_NAME='{}', LAST_NAME='{}',\
+              PHONE_NUMBER={}, EMAIL='{}'".format(\
+              fName, lName, phone, email)
+
+    sql_str = "UPDATE PHONE_BOOK set \
+       {} where ID={};".format(val_str,change_id)
+    print sql_str
+
+    conn.execute(sql_str)
+    conn.commit()
+    return conn.total_changes
+
+def deleteContact(change_id):
+    # Create sql string
+    sql_str = "DELETE from PHONE_BOOK where ID={}"\
+             .format(change_id)
+    conn.execute(sql_str)
+    conn.commit()
+    return conn.total_changes
+
+
+
